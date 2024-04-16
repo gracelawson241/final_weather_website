9f3ccb3035b14f7c11e2f630133579c18065999c
Grace <gracelawson241@gmail.com>
2024-04-15 20:26:32 -0400

    fixed spacing problem with certain window sizes in main weather information and began connecting to api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "faa6632e6o4d4eb073bt724d3e3780ff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  //get temperature, wind speed, and humidity levels
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function cityReplacer(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector(".city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
-let searchedCity = document.querySelector("#search-form");
-searchedCity.addEventListener("submit", cityReplacer);
+let user_input_city = document.querySelector("#search-form");
+user_input_city.addEventListener("submit", cityReplacer);
