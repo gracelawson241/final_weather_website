6b6780a98e3e16e4402fd14985ff52f581723238
Grace <gracelawson241@gmail.com>
2024-04-15 21:04:40 -0400

    finished refreshWeather function to change desired html properties, added a formatDate function, inc. emoji size, removed placeholder values in html

--- src/app.js
+++ src/app.js
@@ -1,5 +1,48 @@
 function refreshWeather(response) {
-  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  //unchecked
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let date = new Date(response.data.time * 1000);
+
+  let windSpeedElement = document.querySelector("#windSpeed");
+  let timeElement = document.querySelector("#time");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mi/h`;
+  timeElement.innerHTML = formatDate(date);
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
